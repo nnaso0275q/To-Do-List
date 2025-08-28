359c6ebdcf4582d83adec02b5b1f47ff60ed4421
25LP9779 25LP9779 <25LP9779@G301-CC0199.localdomain>
2025-08-28 10:16:07 +0800

    aldaa zaslaa

--- Desktop/react1/src/components/filterButton.jsx
+++ Desktop/react1/src/components/filterButton.jsx
@@ -1,13 +1,11 @@
-import { Children } from "react"
 
- export const FilterButton=(onClick)=>{
+ export const FilterButton = ({onClick, active, children})=>{
     return(
         <button
         onClick={onClick}
-        className={"px-3 h-8 rounded" `${
+        className={`px-3 h-8 rounded ${
         active ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-600"
       }`}
-        >{Children}</button>
-      
+        >{children}</button>
     )
  } 
